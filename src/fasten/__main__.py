244fe7e3c357dca481dec0ef006e18fa7ccc67c1
Michele Scarlato <michele.scarlato@gmail.com>
2022-05-25 08:59:10 +0200

    Create directories for CG and SCG it not exists.

--- src/fasten/__main__.py
+++ src/fasten/__main__.py
@@ -1,12 +1,13 @@
 import argparse
-from fasten.readRequirementsFile import ReadRequirementsFile
-from fasten.checkPackageAvailability import CheckPackageAvailability
-from fasten.createCallGraph import CreateCallGraph
-from fasten.requestFasten import RequestFasten
-from fasten.stitchCallGraph import StitchCallGraph
-from fasten.createAdjacencyList import CreateAdjacencyList
-from fasten.enrichCallGraph import EnrichCallGraph
-from fasten.stitchedCallGraphAnalyzer import StitchedCallGraphAnalyzer
+from readRequirementsFile import ReadRequirementsFile
+from checkPackageAvailability import CheckPackageAvailability
+from createCallGraph import CreateCallGraph
+from requestFasten import RequestFasten
+from stitchCallGraph import StitchCallGraph
+from createAdjacencyList import CreateAdjacencyList
+from enrichCallGraph import EnrichCallGraph
+from stitchedCallGraphAnalyzer import StitchedCallGraphAnalyzer
+from createDirectories import CreateDirectories
 
 
 def main():
@@ -18,16 +19,17 @@ def main():
     parser.add_argument("--timestamp", type=int, help="Timestamp of the package's version") # 42
     parser.add_argument("--version", type=str, help="Version of the product") # 1.0
     parser.add_argument("--requirements", type=str, help="Path to the requirements file") # /mnt/stuff/projects/work/pypi-plugin/requirements.txt
-    parser.add_argument("--cg_path", type=str, help="Path where the Call Graphs will be stored")
-    parser.add_argument("--scg_path", type=str, help="Path where the Stitched Call Graph will be stored")
+    parser.add_argument("--cg_path", type=str, help="Path where the Call Graphs will be stored. Will be created if not exists.")
+    parser.add_argument("--scg_path", type=str, help="Path where the Stitched Call Graph will be stored. Will be created if not exits.")
     args = parser.parse_args()
-
     url = 'https://api.fasten-project.eu/api/pypi/' # URL to the FASTEN API
     forge = "local" # Source the product was downloaded from
     max_iter = -1 # Maximum number of iterations through source code (from pycg).
     operation = "call-graph" # or key-error for key error detection on dictionaries (from pycg).
     call_graphs = []
     vulnerabilities = []
+
+    CreateDirectories.DirectoryCheck(args.cg_path, args.scg_path) # Create directories to store the Call Graphs and the Stitched Call Graph
 
     pkgs = ReadRequirementsFile.readFile(args.requirements) # Read requirements.txt
     pkgs, unknown_pkgs = CheckPackageAvailability.checkPackageAvailability(pkgs, url) # Check if packages are known by FASTEN
