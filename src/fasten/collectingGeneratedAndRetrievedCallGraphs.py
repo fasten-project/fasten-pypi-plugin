2b5db817a5f8dacbd7c9f4f245ee2ad64906ec81
Michele Scarlato <michele.scarlato@endocode.com>
2022-06-15 13:02:04 +0200

    Update collectingGeneratedAndRetrievedCallGraphs.py
    Including in the local call graph generation also packages with 500 responses from the FASTEN server.

--- src/fasten/collectingGeneratedAndRetrievedCallGraphs.py
+++ src/fasten/collectingGeneratedAndRetrievedCallGraphs.py
@@ -6,7 +6,8 @@ def collectingGeneratedAndRetrievedCallGraphs(args, all_pkgs, url):
     deleteCallGraphsDir(CallGraphsDirLocal)
     print("CALL GRAPHS Retrieval:")
     ReceivedCallGraphsLocation, known_call_graphs, unknown_call_graphs, call_graphs_connectivity_issues = RequestFastenKnownAndUnknownLists.requestFastenKnownAndUnknownLists(args, all_pkgs, url, "rcg")
-    GeneratedCallGraphPaths = executeCallGraphGenerator(unknown_call_graphs, args.fasten_data)#,CallGraphsDirLocal)
+    unknown_call_graphs_and_connectivity_issues = {**unknown_call_graphs, **call_graphs_connectivity_issues}
+    GeneratedCallGraphPaths = executeCallGraphGenerator(unknown_call_graphs_and_connectivity_issues, args.fasten_data)#,CallGraphsDirLocal)
     # merging lists of retrieved and generated call graphs location
     CallGraphsList = GeneratedCallGraphPaths + ReceivedCallGraphsLocation
     return CallGraphsList
